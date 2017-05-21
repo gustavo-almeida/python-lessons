c0e0d943012f6f9a2b420c45981cf2b9730f95c4
Gustavo Almeida <gustavo.almeida@intercase.com.br>
2017-05-21 10:44:21 -0300

    Refatorado bhaskara.py

--- bhaskara.py
+++ bhaskara.py
@@ -1,18 +1,23 @@
 import math
 
-a = float(input("Qual o valor de A da equação de segundo grau? "))
-b = float(input("Qual o valor de B da equação de segundo grau? "))
-c = float(input("Qual o valor de C da equação de segundo grau? "))
+def delta(a,b,c):
+    return (b ** 2) - 4 * a * c
 
-delta = (b ** 2) - 4 * a * c
+def main():
+    a = float(input("Qual o valor de A da equação de segundo grau? "))
+    b = float(input("Qual o valor de B da equação de segundo grau? "))
+    c = float(input("Qual o valor de C da equação de segundo grau? "))
+    imprime_raizes(a,b,c)
 
-if delta == 0:
-    raiz = -b / (2 * a)
-    print ("Essa equação possui raízes iguais: ", raiz)
-elif delta > 0:
-    raizDelta = math.sqrt(delta)
-    raiz1 = (-b + raizDelta) / (2 * a)
-    raiz2 = (-b - raizDelta) / (2 * a)
-    print ("Essa equação possui duas raízes diferentes: ", raiz1, "e", raiz2)
-else:
-    print ("Essa equação não tem raízes reais.")
+def imprime_raizes(a,b,c):
+    d = delta(a,b,c)
+    if d == 0:
+        raiz = -b / (2 * a)
+        print ("Essa equação possui raízes iguais: ", raiz)
+    elif d > 0:
+        raizDelta = math.sqrt(d)
+        raiz1 = (-b + raizDelta) / (2 * a)
+        raiz2 = (-b - raizDelta) / (2 * a)
+        print ("Essa equação possui duas raízes diferentes: ", raiz1, "e", raiz2)
+    else:
+        print ("Essa equação não tem raízes reais.")
