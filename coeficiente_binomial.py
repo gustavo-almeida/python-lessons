7d00c90d4894a2236faeca37e4e251149f6cee85
Gustavo Almeida <gustavo.almeida@intercase.com.br>
2017-05-20 23:13:57 -0300

    corrigido coeficiente binomial

--- coeficiente_binomial.py
+++ coeficiente_binomial.py
@@ -1,31 +1,35 @@
-def fatorial(valor):
-    produto_total = valor
-    while valor > 1:
-        anterior = valor-1
-        produto_total = produto_total * anterior
-        valor = valor-1
-    return produto_total
+def fatorial(n):
+    fat = 1
+    while n > 1:
+        fat = fat * n
+        n = n - 1
+    return fat
+
+###### VERSÃO INICIAL NÃO FUNCIONA COM 0
+    # produto_total = valor
+    # while valor > 1:
+    #     anterior = valor-1
+    #     produto_total = produto_total * anterior
+    #     valor = valor-1
+    # return produto_total
 
 def testa_fatorial():
     if fatorial(1) == 1:
         print("Funciona para 1")
     else:
-        print("Não funciona para 1")    
-
+        print("Não funciona para 1")
     if fatorial(2) == 2:
         print("Funciona para 2")
     else:
         print("Não funciona para 2")
-    
     if fatorial(0) == 1:
         print("Funciona para 0")
     else:
         print("Não funciona para 0")
-    
     if fatorial(5) == 120:
         print("Funciona para 5")
     else:
         print("Não funciona para 5")
 
-def numero_binomial(n, p):
+def binomial(n, p):
     return fatorial(n) / (fatorial(n-p) * fatorial(p))
